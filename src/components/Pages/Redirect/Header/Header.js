00e40c10f96cb1922ea76c20880fefaad491c360
tuyennv <tuyennv@rikkeisoft.com>
2020-06-25 09:39:16 +0700

    update header

--- src/components/Pages/Redirect/Header/Header.js
+++ src/components/Pages/Redirect/Header/Header.js
@@ -1,27 +1,60 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import './Header.css'
 
-import { withStyles } from '@material-ui/core/styles'
-import { makeStyles } from '@material-ui/core/styles'
+import { fade, withStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
+import InputBase from '@material-ui/core/InputBase'
 import MenuIcon from '@material-ui/icons/Menu'
+import SearchIcon from '@material-ui/icons/Search'
 
-const useStyles = makeStyles(theme => ({
+const useStyles = theme => ({
     root: {
         flexGrow: 1,
+    },
+    header: {
+        color: theme.palette.common.black,
+        backgroundColor: theme.palette.common.white,
+
+    },
+    search: {
+        position: 'relative',
+        flexGrow: 1,
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: theme.palette.common.white,
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.black, 0.05),
+        }
+    },
+    searchIcon: {
+        position: 'absolute',
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 2),
+    },
+    searchInputRoot: {
+        color: 'inherit',
+        width: '100%',
+    },
+    searchInputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        paddingLeft: `calc(1em + ${theme.spacing(5)}px)`,
     },
     menuButton: {
         marginRight: theme.spacing(2),
     },
-    title: {
-        flexGrow: 1,
+    actionButton: {
+        textTransform: 'capitalize',
+        minWidth: '100px',
+    },
+    joinButton: {
+        
     }
-}))
+})
 
 class Header extends React.Component {
     constructor (props) {
@@ -30,26 +63,27 @@ class Header extends React.Component {
     }
 
     render = () => (
-        <div className={"header-panel" + this.props.classes.root}>
-            <AppBar position="static">
+        <div className={this.props.classes.root}>
+            <AppBar position="static" className={this.props.classes.header}>
                 <Toolbar>
                     <IconButton edge="start" className={this.props.classes.menuButton} color="inherit">
                         <MenuIcon />
                     </IconButton>
-                    <Typography variant="h6" className={this.props.classes.title}>
-                        News
-                    </Typography>
-                    <Button color="inherit">Login</Button>
+                    <div className={this.props.classes.search}>
+                        <div className={this.props.classes.searchIcon}>
+                            <SearchIcon />
+                        </div>
+                        <InputBase placeholder="Search"
+                            classes={{
+                                root: this.props.classes.searchInputRoot,
+                                input: this.props.classes.searchInputInput,
+                            }}
+                        />
+                    </div>
+                    <Button color="inherit" className={this.props.classes.actionButton}>Topics</Button>
+                    <Button color="inherit" className={this.props.classes.actionButton}>Login</Button>
+                    <Button color="inherit" className={this.props.classes.actionButton}>Join free</Button>
                 </Toolbar>
-            {/* <Link to="/t" className="header-item">
-                <div>Topics</div>
-            </Link>
-            <Link to="/login" className="header-item">
-                <div>Login</div>
-            </Link>
-            <Link to="/join" className="header-item">
-                <button>Join free</button>
-            </Link> */}
             </AppBar>
         </div>
     )
